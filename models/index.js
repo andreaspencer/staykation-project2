876840c583d30fb72dda6afb1f4e83e982cd4da8
Batya <Gigi.ucla21@gmail.com>
2021-11-20 10:10:14 -0800

    create working posts

--- models/index.js
+++ models/index.js
@@ -1,7 +1,15 @@
-// Used for collecting and exporting the User model data
-
+// Collect and export models data
 const User = require('./User'); // Import User model
 const Post = require('./Post'); // Import Post model
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 
 module.exports = { User, Post };
