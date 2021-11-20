dbce69d69d8460319fe8c995d507370feb751925
Batya <Gigi.ucla21@gmail.com>
2021-11-20 09:02:50 -0800

    add Post model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Used for collecting and exporting the User model data
 
 const User = require('./User'); // Import User model
+const Post = require('./Post'); // Import Post model
 
 
-
-module.exports = { User };
+module.exports = { User, Post };
