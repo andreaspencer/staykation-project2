574076a22a2f90d25fe681acbe8455cef4204e5e
Batya <Gigi.ucla21@gmail.com>
2021-11-27 13:59:00 -0800

    add cloudinary

--- server/app.js
+++ server/app.js
@@ -1,9 +1,69 @@
-const express = require('express');
+//const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const mysqlStore = require('connect-mysql')(session);
 const { application } = require('express');
 const auth = require('./lib/auth');
+
+const submitBtn = document.getElementById('submitBtn');
+const fileInput = document.getElementById('fileInput');
+const gallery = document.getElementById('gallery');
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('submitting');
+    const files = [...fileInput.files];
+    files.forEach(files => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            uploadImage(reader.result)
+        };
+        reader.onerror = () => {
+            console.error('Invalid');
+        };
+    }
+        )
+});
+
+const uploadImage = async (base64EncodedImage) => {
+    try {
+        const res = await fetch('/.netlify/functions/upload', {
+            method: 'POST',
+            body: base64EncodedImage
+        })
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+fileInput.addEventListener('change', () => {
+    const files = fileInput.files;
+    handleFiles(files);
+});
+    const handleFiles = () => {
+    gallery.innerHTML = '';
+    const copiedFiles = [...files]
+    copiedFiles.forEach(previewFiles)
+    }
+
+
+const previewFiles = (file) => {
+    const reader = new FileReader(); 
+    reader.readAsDataURL(file);
+    reader.onloadend = (data) => {
+        console.log('Finished loading');
+        const img = document.createElement('img');
+        img.width = 300;
+        img.src = reader.result;
+        gallery.appendChild(img);
+
+    }
+
+}
+
+
 
 // app.use(cookieParser());
 
