7691ec73e1e8c3f230cf9e6b02011797a7fd7702
Batya <Gigi.ucla21@gmail.com>
2021-11-27 14:59:27 -0800

    add cloudinary

--- models/index.js
+++ models/index.js
@@ -1,52 +1,60 @@
-// Collect and export models data
-const User = require('./User'); // Import User model
-const Post = require('./Post'); // Import Post model
-const Vote = require('./Vote'); // Import Vote model
-
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id'
 });
-
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'user_id'
 });
 
-
 User.belongsToMany(Post, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'user_id'
-  });
-  
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
-  Post.belongsToMany(User, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'post_id'
-  });
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
 
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-  Vote.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
 
-  Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
-  
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
 
-  User.hasMany(Vote, {
-    foreignKey: 'user_id'
-  });
-  
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
 
-  Post.hasMany(Vote, {
-    foreignKey: 'post_id'
-  });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
 
-module.exports = { User, Post, Vote };
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+<<<<<<< HEAD
 
 router.post('login', passport.authenticate('local', {
     successRedirect: '/',
@@ -16,8 +17,12 @@ router.get('/logout', (req, res) => {
     req.logout();
     return res.redirect('/');
 });
+=======
+const commentRoutes = require('./comment-routes');
+>>>>>>> 68010ad841546c25ad3525746d6c994108178810
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
